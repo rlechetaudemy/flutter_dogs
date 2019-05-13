3244da8fc58e726aeb07bf5d5d0926d72583bcc3
rlecheta <rlecheta@livetouch.com.br>
2019-05-13 13:20:37 -0300

    exemplo de splash

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -3,12 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_parser_json/pages/dogs_page.dart';
 import 'package:flutter_parser_json/utils/nav.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+
 
     splash(context);
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Container(
       decoration: new BoxDecoration(
         image: new DecorationImage(
